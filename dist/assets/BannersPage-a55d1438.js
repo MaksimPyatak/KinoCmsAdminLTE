2cfb19462d835619b081e251f5eb52b016ef00cc
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-04-30 16:10:21 +0300

    adding dist

---
+++ dist/assets/BannersPage-a55d1438.js
@@ -0,0 +1,1 @@
+import{_ as e,o as n,c,a as o}from"./index-91786309.js";const s={},t=o("h1",null,"Banner",-1),a=[t];function r(_,l){return n(),c("div",null,a)}const f=e(s,[["render",r]]);export{f as default};
