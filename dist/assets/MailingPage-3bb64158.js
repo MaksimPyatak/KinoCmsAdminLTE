6134e3374f387ddf51b5bda7d0334b921ffcfcab
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-05-09 15:11:33 +0300

    created CastomSelect

---
+++ dist/assets/MailingPage-3bb64158.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index-ab0296f1.js";const a={};function o(r,t){return c(),n("div",null," mailing ")}const _=e(a,[["render",o]]);export{_ as default};
--- dist/assets/UsersPage-a4513ea9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as r}from"./index-cebdc9d8.js";const o={};function s(t,n){return c(),r("div")}const _=e(o,[["render",s]]);export{_ as default};
