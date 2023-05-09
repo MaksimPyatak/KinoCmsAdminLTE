498037d4e31769961ab1c68483775b3b5410163d
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-05-09 14:44:47 +0300

    adding dist

--- dist/assets/AllFilms-6557020a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-ed6f955c.js";const r={};function t(n,s){return c(),o("div")}const a=e(r,[["render",t]]);export{a as default};
--- dist/assets/FilmPage-3b4f11f7.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-ed6f955c.js";const r={};function t(n,a){return c(),o("div")}const _=e(r,[["render",t]]);export{_ as default};
