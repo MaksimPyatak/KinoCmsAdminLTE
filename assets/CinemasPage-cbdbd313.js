dd0365e04e44f28b5d1504c02451bf8a23164f20
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-05-10 23:40:47 +0300

    adding dist

--- assets/CinemasPage-cbdbd313.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index-0ada2d6d.js";const o={};function r(t,a){return c(),n("div")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ assets/UsersPage-ef1977bb.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index-3c2f782e.js";const o={};function s(t,n){return c(),r("div")}const _=e(o,[["render",s]]);export{_ as default};
