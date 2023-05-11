cd21a44b859032bf05a0054f017ec475485fab21
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-05-11 17:01:42 +0300

    adding dist

---
+++ dist/assets/AllPromotions-70f049e8.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c}from"./index-d103d357.js";const r={};function t(n,s){return o(),c("div")}const a=e(r,[["render",t]]);export{a as default};
---
+++ dist/assets/UserEditing-93242963.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index-d103d357.js";const t={};function n(o,s){return c(),r("div")}const a=e(t,[["render",n]]);export{a as default};
