6048a47285378bf673742207862b74596308461f
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-05-15 11:40:50 +0300

    add adding image in TroughBanner

---
+++ assets/AllPromotions-f573afe1.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c}from"./index-43bdd8d0.js";const r={};function t(n,s){return o(),c("div")}const a=e(r,[["render",t]]);export{a as default};
--- assets/SelectUsers-14ac6887.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as r}from"./index-d103d357.js";const t={};function o(s,n){return c(),r("div")}const a=e(t,[["render",o]]);export{a as default};
