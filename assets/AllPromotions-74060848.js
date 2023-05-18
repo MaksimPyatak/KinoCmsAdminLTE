a680e62f6f40d00f7a81f6a5a21c9821c09fca5d
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-05-18 23:09:48 +0300

    adding dist

--- assets/AllPromotions-74060848.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c}from"./index-edede0d3.js";const r={};function t(n,s){return o(),c("div")}const a=e(r,[["render",t]]);export{a as default};
---
+++ assets/MailingPage-11b7e464.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index-3630ebfb.js";const a={};function o(r,t){return c(),n("div",null," mailing ")}const _=e(a,[["render",o]]);export{_ as default};
