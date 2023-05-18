0df1ae6a1acacb7ad26382599be627e81293bebb
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-05-18 18:17:57 +0300

    adding dist

--- assets/NewsPage-47e45ac3.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-42e58604.js";const r={};function t(n,s){return c(),o("div")}const _=e(r,[["render",t]]);export{_ as default};
--- assets/UserEditing-df58cd84.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as r}from"./index-42e58604.js";const t={};function n(o,s){return c(),r("div")}const a=e(t,[["render",n]]);export{a as default};
