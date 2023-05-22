c3655850820b5be2546cc6ace8e34cbb447a4827
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-05-22 12:04:09 +0300

    adding validastion to CastomInput

--- assets/CinemasPage-4b4ebb2f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index-a1e74484.js";const o={};function r(t,a){return c(),n("div")}const _=e(o,[["render",r]]);export{_ as default};
--- assets/UsersPage-57b0e4cb.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as r}from"./index-a1e74484.js";const o={};function s(t,n){return c(),r("div")}const _=e(o,[["render",s]]);export{_ as default};
