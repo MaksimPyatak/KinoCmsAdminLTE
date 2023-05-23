edb4d509e9b11719c297ac628a91c7e0c0fe34ff
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-05-23 15:29:13 +0300

    adding LanguageTabs component

---
+++ dist/assets/AllNews-21ee962e.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-35843a8e.js";const r={};function t(n,s){return c(),o("div")}const a=e(r,[["render",t]]);export{a as default};
--- dist/assets/MailingPage-16a9966b.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index-06482017.js";const a={};function o(r,t){return c(),n("div",null," mailing ")}const _=e(a,[["render",o]]);export{_ as default};
