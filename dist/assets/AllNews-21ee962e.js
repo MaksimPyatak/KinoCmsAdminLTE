bd28a83861e5f93c0146b139396aa21e7c1c73c6
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-05-25 16:58:19 +0300

    adding Calendar

--- dist/assets/AllNews-21ee962e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-35843a8e.js";const r={};function t(n,s){return c(),o("div")}const a=e(r,[["render",t]]);export{a as default};
--- dist/assets/UserEditing-0fe01c94.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as r}from"./index-35843a8e.js";const t={};function n(o,s){return c(),r("div")}const a=e(t,[["render",n]]);export{a as default};
